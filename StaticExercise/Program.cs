f991e21e6edc6c091aee9643c9557c107e86eddf
Alex David <alexdavid716@gmail.com>
2024-03-04 10:15:38 -0500

    static exercise

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace StaticExercise
+﻿using System;
+
+class Program
 {
-    public class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        double fahrenheit = 98.6;
+        double celsius = TempConverter.FahrenheitToCelsius(fahrenheit);
+        Console.WriteLine($"{fahrenheit} Fahrenheit is equal to {celsius:F2} Celsius.");
+
+        celsius = 37;
+        fahrenheit = TempConverter.CelsiusToFahrenheit(celsius);
+        Console.WriteLine($"{celsius} Celsius is equal to {fahrenheit:F2} Fahrenheit.");
     }
 }
